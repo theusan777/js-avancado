1df03fb770bc6d260a19577be2eae37bc7a37037
theusan777 <theusant777@icloud.com>
2026-03-03 16:31:58 -0300

    accessing properties in the context of the object.

--- index.js
+++ index.js
@@ -1,34 +1,8 @@
-const obj = {}
-console.log(obj)
-console.log(typeof obj)
-
 const user = {
-  email: "matheus@email.com",
-  age: 21,
-  name: {
-    first_name: "Matheus",
-    surnme: "Santos"
-  }, 
-
-  address: {
-    street: "Rua Z",
-    number: 23,
-    city: "São Paulo",
-    postal_code: "12345-552"
-  }, 
-
-  message: function(){
-    console.log("Oi, Matheus");
-    
-  }
-  
+  name: "Matheus",
+  message: function () {
+    console.log(`Hello, ${this.name}!`)
+  },
 }
 
- console.log(user);
- console.log(user.email);
- console.log(user.name.first_name);
- user.message()
- console.log(user["age"]);
- 
- 
- 
+user.message()
