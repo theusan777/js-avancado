a8ea90683f18d3427fb7364cd1b563d0c6afb505
theusan777 <theusant777@icloud.com>
2026-03-03 17:40:24 -0300

    constructor function

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-let content = undefined
-console.log(content ?? "default value")
+function createProduct(name) {
+  const product = {}
 
-const user = {
-  name: "Karina",
-  avatar: undefined,
+  product.name = name
+  product.details = function() {
+    console.log(`O nome do produto é ${this.name}`)
+  }
+
+  return product
 }
-console.log(user.avatar ?? "default avatar")
+
+const product1 = new createProduct("teclado")
+console.log(product1.name)
+product1.details()
+
+const product2 = new createProduct("mouse")
+console.log(product2.name)
+product2.details()
+
+
+function Person(name) {
+  this.name = name
+  this.message = function() {
+    console.log(`Olá, meu nome é ${this.name}`)
+  }
+}
+
+const person1 = new Person("Karina")
+console.log(person1.name)
+person1.message()
+
+const person2 = new Person("Matheus")
+console.log(person2.name)
+person2.message()
