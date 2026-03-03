2b80d415a3742dede08a3032bef7229c799a1c1b
theusan777 <theusant777@icloud.com>
2026-03-03 16:37:39 -0300

    updating objects

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const user = {
-  name: "Matheus",
-  message: function () {
-    console.log(`Hello, ${this.name}!`)
-  },
+const product = {
+  name: "controle",
+  price: 299.99,
 }
 
-user.message()
+console.log(product.price)
+product.price = 249.99
+console.log(product.price)
+
+console.log(product.name)
+product.name = "controle ps5"
+console.log(product.name)
+
+product["price"] = 199.99
+console.log(product)
