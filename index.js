c096045d65e6c0092be4a6d8d7a807ac84e37d0c
theusan777 <theusant777@icloud.com>
2026-03-04 17:59:48 -0300

    getting string length

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
-function createProduct(name) {
-  const product = {}
+let message = "Estou aprendendo JavaScript!"
+console.log(message.length);
 
-  product.name = name
-  product.details = function() {
-    console.log(`O nome do produto é ${this.name}`)
-  }
 
-  return product
+let password = "minhaSenha123" 
+
+if (password.length >= 8) {
+    console.log("Senha forte")
+}else {    console.log("Senha fraca")
 }
 
-const product1 = new createProduct("teclado")
-console.log(product1.name)
-product1.details()
-
-const product2 = new createProduct("mouse")
-console.log(product2.name)
-product2.details()
-
-
-function Person(name) {
-  this.name = name
-  this.message = function() {
-    console.log(`Olá, meu nome é ${this.name}`)
-  }
-}
-
-const person1 = new Person("Karina")
-console.log(person1.name)
-person1.message()
-
-const person2 = new Person("Matheus")
-console.log(person2.name)
-person2.message()
+let value = 12345
+console.log((value).toString().length);
