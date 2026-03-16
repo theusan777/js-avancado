7d64571ad8e8e493ac1059462b2e51d617ada94e
theusan777 <theusant777@icloud.com>
2026-03-16 12:25:47 -0300

    for

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-let value = 0
+for (step = 0; step < 5; step++) {
+  console.log(step)
+}
 
-do {
-  value++
-  console.log(value)
-} while (value < 10)
+console.log("------------------------------------")
 
-console.log("Executando")
+console.log(`7x0 = ${7*0}`)
+console.log(`7x1 = ${7*1}`)
+console.log(`7x2 = ${7*2}`)
+console.log(`7x3 = ${7*3}`)
+console.log(`7x4 = ${7*4}`)
+console.log(`7x5 = ${7*5}`)
+console.log(`7x6 = ${7*6}`)
+console.log(`7x7 = ${7*7}`)
+console.log(`7x8 = ${7*8}`)
+console.log(`7x9 = ${7*9}`)
+console.log(`7x10 = ${7*10}`)
+
+console.log("-----------------------------------")
+
+let number = 7
+for (step = 0; step <= 10; step++) {
+  console.log(`${number}x${step} = ${number*step}`)
+}
