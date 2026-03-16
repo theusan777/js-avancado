2f02c22ba6ff861f3341087a922a13a5aa5a9ca7
theusan777 <theusant777@icloud.com>
2026-03-16 12:37:01 -0300

    for of

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-let person = {
-  name: "John",
-  surname: "Doe",
-  email: "john.doe@example.com",
-}
+let students = ["John", "Jane", "Doe"]
 
-for (let property in person) {
-  console.log(property + ": " + person[property]);
+for (let student of students) {
+    console.log(student)
 }
