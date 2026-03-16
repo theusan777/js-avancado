c7230e3844a4b9fc1bcef37a30a51fb36ae571c7
theusan777 <theusant777@icloud.com>
2026-03-16 12:15:31 -0300

    do while

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-let execute = true 
+let value = 0
 
-while (execute) {
-    let response = prompt("Deseja continuar? 1 (sim) ou 2 (não)")
-    
-    if (response === "2") {
-      execute = false
-    }
-}
+do {
+  value++
+  console.log(value)
+} while (value < 10)
 
-console.log("Programa encerrado.")
+console.log("Executando")
