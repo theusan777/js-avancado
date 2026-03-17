d58589ba0619c164d35f2fcc51d8331f76f2557e
theusan777 <theusant777@icloud.com>
2026-03-17 10:45:53 -0300

    converting date and time to string

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let date = new Date("2026-03-17T12:00:00")
 
-console.log(date.getDate().toString().padStart(2, "0") + "/" + (date.getMonth() + 1).toString().padStart(2, "0") + "/" + date.getFullYear())
-
-console.log(date.getHours().toString().padStart(2, "0") + ":" + date.getMinutes().toString().padStart(2, "0") + ":" + date.getSeconds().toString().padStart(2, "0"));
-
+console.log(date.toLocaleDateString())
+console.log(date.toLocaleTimeString())
+console.log(date.toLocaleString())
 
