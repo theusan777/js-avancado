7357c78a8cf2719cf9939e277970c5fa879c2945
theusan777 <theusant777@icloud.com>
2026-03-17 10:55:12 -0300

    using toLocalString

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 let date = new Date("2026-03-17T12:00:00")
 
-console.log(date.toLocaleDateString())
-console.log(date.toLocaleTimeString())
 console.log(date.toLocaleString())
-
 console.log(date.toLocaleDateString("en"))
-console.log(date.toLocaleDateString("en-GB"))
-console.log(date.toLocaleDateString("en-US"))
-console.log(date.toLocaleDateString("en", { weekday: "long" }))
-console.log(date.toLocaleDateString("en", { month: "long" }))
-console.log(date.toLocaleDateString("en", { year: "2-digit" }))
+
+console.log(
+  date.toLocaleDateString("pt-BR", {
+    dateStyle: "short"
+  })
+)
+
+console.log(
+  date.toLocaleDateString("pt-BR", {
+    dateStyle: "long"
+  })
+)
+
+console.log(
+  date.toLocaleDateString("pt-BR", {
+    dateStyle: "medium"
+  })
+)
+
+console.log(
+  date.toLocaleDateString("pt-BR", {
+    dateStyle: "full"
+  })
+)
 
 
+console.log(date.toLocaleDateString("pt-BR", {
+  day: "2-digit",
+  month: "2-digit",
+  year: "2-digit"
+}))
 
+let amount = 14.5
+
+console.log(amount.toLocaleString("pt-BR", {
+  style: "currency",
+  currency: "BRL"
+}))
