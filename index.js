0b92ccfa01e48247b3c655234d3ea24520fbec37
theusan777 <theusant777@icloud.com>
2026-03-17 10:43:24 -0300

    formatting date and time

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-let date = new Date("2026-03-17T12:00:00Z")
+let date = new Date("2026-03-17T12:00:00")
 
-date.setDate(date.getDate() + 10)
+console.log(date.getDate().toString().padStart(2, "0") + "/" + (date.getMonth() + 1).toString().padStart(2, "0") + "/" + date.getFullYear())
 
-date.setMonth(date.getMonth() + 6)
-
-date.setFullYear(date.getFullYear() + 2)
-
-date.setHours(date.getHours() + 1)
-
-date.setMinutes(date.getMinutes() + 30)
-
-date.setSeconds(date.getSeconds() + 45)
-
-
-console.log(date.toISOString())
-
-
-
-
+console.log(date.getHours().toString().padStart(2, "0") + ":" + date.getMinutes().toString().padStart(2, "0") + ":" + date.getSeconds().toString().padStart(2, "0"));
 
 
