0828a10605dbbc622fc77053b28d852f17efae69
theusan777 <theusant777@icloud.com>
2026-03-17 10:18:08 -0300

    new date

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-for (let i = 0; i < 10; i++) {
-  if (i === 5) {
-    continue
-  }
+console.log(new Date())
 
-  console.log(i)
-}
+console.log(new Date(1000))
+
+console.log(new Date('2024-06-01T12:00:00Z'))
+
+console.log(new Date('Invalid Date'))
+
