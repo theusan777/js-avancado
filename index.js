06748c1401343a2c557f892a86b0038098f1f138
theusan777 <theusant777@icloud.com>
2026-03-18 11:12:58 -0300

    static method

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 class User {
-  constructor(name, email) {
-    this.name = name;
-    this.email = email;
+  static showMessage() {
+    console.log("Olá a todos")
   }
+}  
 
-  
-  sendEmail(){
-  console.log("E-mail enviado para", this.name, "para o endereço", this.email)
-  }
-}
-
-const user1 = new User("Alice", "alice@example.com")
-user1.sendEmail()
+User.showMessage()
